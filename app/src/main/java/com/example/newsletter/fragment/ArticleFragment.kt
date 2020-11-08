dfba29a9b8f2eeb60840683e7b041bbae6a6df73
1250681923 <59725125+1250681923@users.noreply.github.com>
2020-11-08 23:04:58 +0100

    zhang

--- app/src/main/java/com/example/newsletter/fragment/ArticleFragment.kt
+++ app/src/main/java/com/example/newsletter/fragment/ArticleFragment.kt
@@ -1,13 +1,29 @@
 package com.example.newsletter.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import com.example.newsletter.R
 import com.example.newsletter.data.ArticleRepository
 import com.example.newsletter.models.Article
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ArticleFragment : Fragment() {
+
+    /**
+     * Fonction permettant de définir une vue à attacher à un fragment
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_articles_fragment, container, false)
+    }
     /**
      * Récupère la liste des articles dans un thread secondaire
      */
