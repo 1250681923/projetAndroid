6bfe908b490f8b20cad08c9c192a83df15a23406
1250681923 <59725125+1250681923@users.noreply.github.com>
2020-11-13 14:21:48 +0100

    zhang

--- app/src/main/java/com/example/newsletter/fragment/ArticleFragment.kt
+++ app/src/main/java/com/example/newsletter/fragment/ArticleFragment.kt
@@ -11,12 +11,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsletter.R
 import com.example.newsletter.adapters.ListArticlesAdapter
+import com.example.newsletter.adapters.ListArticlesHandler
 import com.example.newsletter.data.ArticleRepository
 import com.example.newsletter.models.Article
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class ArticleFragment : Fragment() {
+class ArticleFragment : Fragment(){
 
     private lateinit var recyclerView: RecyclerView
 
@@ -50,21 +51,21 @@ class ArticleFragment : Fragment() {
     }
 
 
-//    /**
-//     * Récupère la liste des articles dans un thread secondaire
-//     */
-//    private fun getArticles(){
-//        lifecycleScope.launch(Dispatchers.IO) {
-//            val articles = ArticleRepository.getInstance().getArticles()
-//
-//        }
-//    }
-//
-//    /**
-//     * Rempli le recyclerview avec les données récupérées dans le web service
-//     * Cette action doit s'effectuer sur le thread principale
-//     * Car on ne peut mas modifier les éléments de vue dans un thread secondaire
-//     */
+    /**
+     * Récupère la liste des articles dans un thread secondaire
+     */
+    private fun getArticles(){
+        lifecycleScope.launch(Dispatchers.IO) {
+            val articles = ArticleRepository.getInstance().getArticles()
+
+        }
+    }
+
+    /**
+     * Rempli le recyclerview avec les données récupérées dans le web service
+     * Cette action doit s'effectuer sur le thread principale
+     * Car on ne peut mas modifier les éléments de vue dans un thread secondaire
+     */
     private fun bindData(articles: List<Article> ) {
         lifecycleScope.launch(Dispatchers.Main) {
             val adapter = ListArticlesAdapter(articles)
